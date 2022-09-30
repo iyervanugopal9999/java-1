a8d1ac5454e906dfd75f2bd0c4933a17904ba0cf
iyervanugopal9999 <91649338+iyervanugopal9999@users.noreply.github.com>
2022-10-01 01:25:27 +0530

    java commit for dws

--- SpringrestApplicationTests.java
+++ SpringrestApplicationTests.java
@@ -5,6 +5,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringrestApplicationTests {
+	
+	int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
 
 	@Test
 	void contextLoads() {
